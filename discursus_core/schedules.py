f820a86097ddc498422b4d8151281874de9abcde
Olivier Dupuis <odupuis@lantrns.co>
2022-02-09 15:20:39 -0500

    Decreasing frequency of ml training engine job to every half hour

--- discursus_core/schedules.py
+++ discursus_core/schedules.py
@@ -3,5 +3,5 @@ from jobs import mine_gdelt_data, get_enriched_mined_data, feed_ml_trainer_engin
 
 mine_gdelt_data_schedule = ScheduleDefinition(job = mine_gdelt_data, cron_schedule = "2,17,32,47 * * * *")
 get_enriched_mined_data_schedule = ScheduleDefinition(job = get_enriched_mined_data, cron_schedule = "7,22,37,52 * * * *")
-feed_ml_trainer_engine_schedule = ScheduleDefinition(job = feed_ml_trainer_engine, cron_schedule = "10,25,40,55 * * * *")
+feed_ml_trainer_engine_schedule = ScheduleDefinition(job = feed_ml_trainer_engine, cron_schedule = "28,58 * * * *")
 build_data_warehouse_schedule = ScheduleDefinition(job = build_data_warehouse, cron_schedule = "15 3,9,15,21 * * *")
