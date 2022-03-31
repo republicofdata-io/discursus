984084e8bd7f2dd169d9bc4d75b74cb3fa189de7
Olivier Dupuis <odupuis@lantrns.co>
2022-03-31 15:26:10 -0400

    Upgrading dbt and dagster + Sourcing and exposing protest entity

---
+++ discursus_core/dw/models/staging/stitch/stg__stitch__protest_groupings.sql
@@ -0,0 +1,20 @@
+with source as (
+
+    select * from {{ source('stitch', 'protest_groupings') }}
+
+),
+
+base as (
+
+    select
+        cast(protest_name as varchar) as protest_name,
+        cast(published_date_start as date) as published_date_start,
+        cast(published_date_end as date) as published_date_end,
+        cast(countries as varchar) as countries,
+        cast(page_description as varchar) as page_description
+    
+    from source
+
+)
+
+select * from base
