d8ec398602316667273aaf5a7ee0da1f37ec3b07
Olivier Dupuis <odupuis@lantrns.co>
2022-05-14 09:23:25 -0400

    Increasing frequency and number of protest articles sent to ml engine

--- discursus_core/schedules.py
+++ discursus_core/schedules.py
@@ -4,7 +4,7 @@ from jobs import mine_gdelt_data, get_enriched_mined_data, build_data_warehouse,
 mine_gdelt_data_schedule = ScheduleDefinition(job = mine_gdelt_data, cron_schedule = "2,17,32,47 * * * *")
 
 get_enriched_mined_data_schedule = ScheduleDefinition(job = get_enriched_mined_data, cron_schedule = "7,22,37,52 * * * *")
-feed_ml_trainer_engine_schedule = ScheduleDefinition(job = feed_ml_trainer_engine, cron_schedule = "28,58 * * * *")
+feed_ml_trainer_engine_schedule = ScheduleDefinition(job = feed_ml_trainer_engine, cron_schedule = "13,28,43,58 * * * *")
 
 @schedule(job=build_data_warehouse, cron_schedule="15 3,9,15,21 * * *")
 def build_data_warehouse_schedule(context: ScheduleEvaluationContext):
