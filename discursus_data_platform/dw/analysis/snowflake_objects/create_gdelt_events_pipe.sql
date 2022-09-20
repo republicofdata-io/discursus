9e72116b5cf4557610007071e60207b75731d9c0
Olivier Dupuis <odupuis@lantrns.co>
2022-09-20 14:36:44 -0400

    Handling ml enrichments for empty datasets

--- discursus_data_platform/dw/analysis/snowflake_objects/create_gdelt_events_pipe.sql
+++ discursus_data_platform/dw/analysis/snowflake_objects/create_gdelt_events_pipe.sql
@@ -1,6 +1,6 @@
 create pipe gdelt_events_pipe as
 copy into gdelt_events
   from @s3_dio_sources/gdelt
-  file_format = gdelt_csv
+  file_format = csv
   pattern='.*.export.CSV'
   on_error = 'skip_file';
