3e5dd0cbd9a0133185551943224d44f29af3f01e
Olivier Dupuis <odupuis@lantrns.co>
2023-01-25 15:02:48 -0500

    Bridge table between events and observations

--- discursus_data_platform/dp_data_warehouse/models/warehouse/core/observations_fct.sql
+++ discursus_data_platform/dp_data_warehouse/models/warehouse/core/observations_fct.sql
@@ -16,17 +16,8 @@ final as (
     select distinct
         {{ dbt_utils.generate_surrogate_key([
             'published_date',
-            'movement_name',
-            'action_geo_latitude',
-            'action_geo_longitude',
             'observation_url'
-        ]) }} as observation_pk, 
-        {{ dbt_utils.generate_surrogate_key([
-            'published_date',
-            'movement_name',
-            'action_geo_latitude',
-            'action_geo_longitude'
-        ]) }} as event_fk,
+        ]) }} as observation_pk,
 
         published_date,
 
