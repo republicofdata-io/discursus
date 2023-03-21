2e8f529d141a6426827d6aab5b8aaa5523e385ac
Olivier Dupuis <odupuis@lantrns.co>
2023-03-21 15:50:08 -0400

    Moving where to install SAF packages

--- dagster_cloud_post_install.sh
+++ dagster_cloud_post_install.sh
@@ -2,5 +2,10 @@ apt-get update
 apt-get install -y --no-install-recommends git
 export PATH=$PATH:/usr/bin
 
+pip install git+https://github.com/discursus-data/saf_aws.git@0.2#egg=saf-aws
+pip install git+https://github.com/discursus-data/saf_gdelt.git@0.2#egg=saf-gdelt
+pip install git+https://github.com/discursus-data/saf_novacene.git@0.2.1#egg=saf-novacene
+pip install git+https://github.com/discursus-data/saf_web_scraper.git@0.2#egg=saf-web-scraper
+
 cd ./discursus_data_platform/dp_data_warehouse/; 
 dbt deps --profiles-dir ./config/
--- setup.py
+++ setup.py
@@ -3,5 +3,14 @@ from setuptools import find_packages, setup
 if __name__ == "__main__":
     setup(
         name="discursus_data_platform",
-        packages=find_packages()
+        packages=find_packages(),
+        install_requires=[
+            "dagster==1.2.2",
+            "dagster-cloud==1.2.2",
+            "dagster-dbt==0.18.2",
+            "dagster-aws==0.18.2",
+            "dagster-hex==0.1.2",
+            "dbt-core==1.4.5",
+            "dbt-snowflake==1.4.2"
+        ]
     )
