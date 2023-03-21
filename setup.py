14abe24ddada3807714909f7a1265f91a2a75094
Olivier Dupuis <odupuis@lantrns.co>
2023-03-21 16:01:14 -0400

    Adding pandas libraries

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ if __name__ == "__main__":
         name="discursus_data_platform",
         packages=find_packages(),
         install_requires=[
+            "pandas>=1.5.2",
             "dagster==1.2.2",
             "dagster-cloud==1.2.2",
             "dagster-dbt==0.18.2",
             "dagster-aws==0.18.2",
+            "dagster-pandas==0.18.2",
             "dagster-hex==0.1.2",
             "dbt-core==1.4.5",
             "dbt-snowflake==1.4.2"
