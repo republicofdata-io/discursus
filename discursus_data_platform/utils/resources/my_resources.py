f251e4a34cca9287553bb59b5e7b2ec0fb7f4ed4
Olivier Dupuis <odupuis@lantrns.co>
2023-05-09 15:04:59 -0400

    Extracting entities using spacy

--- discursus_data_platform/utils/resources/my_resources.py
+++ discursus_data_platform/utils/resources/my_resources.py
@@ -5,7 +5,6 @@ from dagster_hex.resources import hex_resource
 
 from saf_aws import aws_resource
 from saf_gdelt import gdelt_resource
-from saf_novacene import novacene_resource
 from saf_web_scraper import web_scraper_resource
 from saf_openai import openai_resource
 
@@ -16,11 +15,6 @@ DBT_PROJECT_DIR = file_relative_path(__file__, "./../../dp_data_warehouse/")
 snowflake_configs = config_from_pkg_resources(
     pkg_resource_defs=[
         ('discursus_data_platform.utils.configs', 'snowflake_configs.yaml')
-    ],
-)
-novacene_configs = config_from_pkg_resources(
-    pkg_resource_defs=[
-        ('discursus_data_platform.utils.configs', 'novacene_configs.yaml')
     ],
 )
 hex_configs = config_from_pkg_resources(
@@ -39,7 +33,6 @@ my_snowflake_resource = snowflake_resource.configured(snowflake_configs)
 my_dbt_resource = dbt_cli_resource.configured({
     "profiles_dir": DBT_PROFILES_DIR, 
     "project_dir": DBT_PROJECT_DIR})
-my_novacene_resource = novacene_resource.initiate_novacene_resource.configured(novacene_configs)
 my_openai_resource = openai_resource.initiate_openai_resource.configured(openai_configs)
 my_aws_resource = aws_resource.initiate_aws_resource.configured(None)
 my_web_scraper_resource = web_scraper_resource.initiate_web_scraper_resource.configured(None)
