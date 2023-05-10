cb653350ec5f1e1b73ba17efcf54b6f8702afa21
Olivier Dupuis <odupuis@lantrns.co>
2023-05-10 09:29:45 -0400

    Adding city and state info

--- discursus_data_platform/dp_data_warehouse/models/integration/events/int__events__1_base.sql
+++ discursus_data_platform/dp_data_warehouse/models/integration/events/int__events__1_base.sql
@@ -12,7 +12,7 @@ s_countries as (
 
 {% set partition_window = 'published_date, action_geo_latitude, action_geo_longitude' %}
 
-final as (
+get_unique_geo as (
 
     select distinct
         s_gdelt_events.gdelt_event_natural_key,
@@ -25,6 +25,36 @@ final as (
 
     from s_gdelt_events
     left join s_countries on s_gdelt_events.action_geo_country_code = s_countries.country_code
+
+),
+
+extract_state_city as (
+
+    select distinct
+        gdelt_event_natural_key,
+        published_date,
+        action_geo_full_name,
+        action_geo_country_code,
+        action_geo_country_name,
+        case
+            when regexp_count(action_geo_full_name, ',') = 1 then trim(split_part(action_geo_full_name, ',', 1))
+            when regexp_count(action_geo_full_name, ',') = 2 then trim(split_part(action_geo_full_name, ',', 2))
+            else ''
+        end as action_geo_state_name,
+        case
+            when regexp_count(action_geo_full_name, ',') = 2 then trim(split_part(action_geo_full_name, ',', 1))
+            else ''
+        end as action_geo_city_name,
+        action_geo_latitude,
+        action_geo_longitude
+
+    from get_unique_geo
+
+),
+
+final as (
+
+    select * from extract_state_city
 
 )
 
