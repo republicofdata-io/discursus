d3fafcfd776fec1092ab22bab3910b091009469f
Olivier Dupuis <odupuis@lantrns.co>
2023-07-13 14:44:45 -0400

    Bringing in web_scraper resource + fixing issue with unreadeable url

--- dagster_cloud_post_install.sh
+++ dagster_cloud_post_install.sh
@@ -4,7 +4,6 @@ export PATH=$PATH:/usr/bin
 
 pip install git+https://github.com/discursus-data/saf_aws.git@0.2#egg=saf-aws
 pip install git+https://github.com/discursus-data/saf_gdelt.git@0.2.1#egg=saf-gdelt
-pip install git+https://github.com/discursus-data/saf_web_scraper.git@0.3.1#egg=saf-web-scraper
 pip install git+https://github.com/discursus-data/saf_openai.git@0.1#egg=saf-openai
 
 python -m spacy download en_core_web_sm
